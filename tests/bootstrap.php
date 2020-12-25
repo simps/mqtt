6b49ca507cf40cdfe2379bf672eec8c8143b7ca1
sy-records <52o@qq52o.cn>
2020-12-25 08:52:29 +0800

    Add unit tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of Simps
+ *
+ * @link     https://github.com/simps/mqtt
+ * @contact  Lu Fei <lufei@simps.io>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code
+ */
+
+declare(strict_types=1);
+
+require_once dirname(__DIR__) . '/examples/bootstrap.php';
