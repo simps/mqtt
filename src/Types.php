0176469b93ffba1343b0aeac3b500b46d0537aaf
sy-records <52o@qq52o.cn>
2021-01-18 14:25:05 +0800

    Add visibility on all constants

--- src/Types.php
+++ src/Types.php
@@ -15,33 +15,33 @@ namespace Simps\MQTT;
 
 class Types
 {
-    const CONNECT = 1; // Client request to connect to Server
+    public const CONNECT = 1; // Client request to connect to Server
 
-    const CONNACK = 2; // Connect acknowledgment
+    public const CONNACK = 2; // Connect acknowledgment
 
-    const PUBLISH = 3; // Publish message
+    public const PUBLISH = 3; // Publish message
 
-    const PUBACK = 4; // Publish acknowledgment
+    public const PUBACK = 4; // Publish acknowledgment
 
-    const PUBREC = 5; // Publish received (assured delivery part 1)
+    public const PUBREC = 5; // Publish received (assured delivery part 1)
 
-    const PUBREL = 6; // Publish release (assured delivery part 2)
+    public const PUBREL = 6; // Publish release (assured delivery part 2)
 
-    const PUBCOMP = 7; // Publish complete (assured delivery part 3)
+    public const PUBCOMP = 7; // Publish complete (assured delivery part 3)
 
-    const SUBSCRIBE = 8; // Client subscribe request
+    public const SUBSCRIBE = 8; // Client subscribe request
 
-    const SUBACK = 9; // Subscribe acknowledgment
+    public const SUBACK = 9; // Subscribe acknowledgment
 
-    const UNSUBSCRIBE = 10; // Unsubscribe request
+    public const UNSUBSCRIBE = 10; // Unsubscribe request
 
-    const UNSUBACK = 11; // Unsubscribe acknowledgment
+    public const UNSUBACK = 11; // Unsubscribe acknowledgment
 
-    const PINGREQ = 12; // PING request
+    public const PINGREQ = 12; // PING request
 
-    const PINGRESP = 13; // PING response
+    public const PINGRESP = 13; // PING response
 
-    const DISCONNECT = 14; // Client is disconnecting
+    public const DISCONNECT = 14; // Client is disconnecting
 
-    const AUTH = 15; // Authentication exchange
+    public const AUTH = 15; // Authentication exchange
 }
