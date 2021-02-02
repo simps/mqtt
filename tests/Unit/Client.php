ad4a4657cef39bf4362223217fb69f1979b729df
sy-records <52o@qq52o.cn>
2021-02-02 15:05:46 +0800

    Add a Base64 test

--- tests/Unit/Client.php
+++ tests/Unit/Client.php
@@ -16,6 +16,8 @@ namespace SimpsTest\MQTT\Unit;
 use PHPUnit\Framework\TestCase;
 use Simps\MQTT\Client as MQTTClient;
 use Simps\MQTT\Exception\ConnectException;
+use Simps\MQTT\Protocol\Types;
+use Swoole\Coroutine;
 
 /**
  * @internal
@@ -31,5 +33,27 @@ class Client extends TestCase
         } catch (\Throwable $ex) {
             $this->assertInstanceOf(ConnectException::class, $ex);
         }
+    }
+
+    public function testBase64()
+    {
+        Coroutine::create(function () {
+            $topic = 'simps-mqtt/test/base64';
+            $base64 = base64_encode(file_get_contents(TESTS_DIR . '/files/wechat.jpg'));
+            $client = new MQTTClient(SIMPS_MQTT_REMOTE_HOST, SIMPS_MQTT_PORT, getTestConnectConfig());
+            $client->connect(false);
+            $client->subscribe([$topic => 0]);
+
+            Coroutine::create(function () use ($topic, $base64) {
+                $client = new MQTTClient(SIMPS_MQTT_REMOTE_HOST, SIMPS_MQTT_PORT, getTestConnectConfig());
+                $client->connect();
+                $client->publish($topic, $base64);
+            });
+
+            $buffer = $client->recv();
+            $this->assertSame($buffer['type'], Types::PUBLISH);
+            $this->assertSame($buffer['topic'], $topic);
+            $this->assertSame(strlen($buffer['message']), strlen($base64));
+        });
     }
 }
