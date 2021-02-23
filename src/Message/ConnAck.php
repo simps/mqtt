700a6c968a17fc6dd25997a31ce567a63885fb88
sy-records <52o@qq52o.cn>
2021-02-23 11:47:48 +0800

    Add CONNACK & PUBLISH & PINGRESP Message
    * [x] CONNACK
    * [x] PUBLISH
    * [x] PINGRESP

---
+++ src/Message/ConnAck.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * This file is part of Simps
+ *
+ * @link     https://github.com/simps/mqtt
+ * @contact  Lu Fei <lufei@simps.io>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code
+ */
+
+declare(strict_types=1);
+
+namespace Simps\MQTT\Message;
+
+use Simps\MQTT\Protocol\Types;
+use Simps\MQTT\Protocol\V3;
+use Simps\MQTT\Protocol\V5;
+
+class ConnAck extends AbstractMessage
+{
+    protected $code = 0;
+
+    protected $sessionPresent = 0;
+
+    public function getCode(): int
+    {
+        return $this->code;
+    }
+
+    public function setCode(int $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getSessionPresent(): int
+    {
+        return $this->sessionPresent;
+    }
+
+    public function setSessionPresent(int $sessionPresent): self
+    {
+        $this->sessionPresent = $sessionPresent;
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        $buffer = [
+            'type' => Types::CONNACK,
+            'code' => $this->getCode(),
+            'session_present' => $this->getSessionPresent(),
+        ];
+
+        if ($this->isMQTT5()) {
+            $buffer['properties'] = $this->getProperties();
+
+            return V5::pack($buffer);
+        }
+
+        return V3::pack($buffer);
+    }
+}
---
+++ src/Message/Publish.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * This file is part of Simps
+ *
+ * @link     https://github.com/simps/mqtt
+ * @contact  Lu Fei <lufei@simps.io>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code
+ */
+
+declare(strict_types=1);
+
+namespace Simps\MQTT\Message;
+
+use Simps\MQTT\Protocol\ProtocolInterface;
+use Simps\MQTT\Protocol\Types;
+use Simps\MQTT\Protocol\V3;
+use Simps\MQTT\Protocol\V5;
+
+class Publish extends AbstractMessage
+{
+    protected $topic;
+
+    protected $message;
+
+    protected $qos = ProtocolInterface::MQTT_QOS_0;
+
+    protected $dup = 0;
+
+    protected $retain = 0;
+
+    protected $messageId;
+
+    public function getTopic(): string
+    {
+        return $this->topic;
+    }
+
+    public function setTopic(?string $topic): self
+    {
+        $this->topic = $topic;
+
+        return $this;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(?string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getQos(): int
+    {
+        return $this->qos;
+    }
+
+    public function setQos(int $qos): self
+    {
+        $this->qos = $qos;
+
+        return $this;
+    }
+
+    public function getDup(): int
+    {
+        return $this->dup;
+    }
+
+    public function setDup(int $dup): self
+    {
+        $this->dup = $dup;
+
+        return $this;
+    }
+
+    public function getRetain(): int
+    {
+        return $this->retain;
+    }
+
+    public function setRetain(int $retain): self
+    {
+        $this->retain = $retain;
+
+        return $this;
+    }
+
+    public function getMessageId(): ?int
+    {
+        return $this->messageId;
+    }
+
+    public function setMessageId(?int $messageId): self
+    {
+        $this->messageId = $messageId;
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        $buffer = [
+            'type' => Types::PUBLISH,
+            'topic' => $this->getTopic(),
+            'message' => $this->getMessage(),
+            'dup' => $this->getDup(),
+            'qos' => $this->getQos(),
+            'retain' => $this->getRetain(),
+            'message_id' => $this->getMessageId(),
+        ];
+
+        if ($this->isMQTT5()) {
+            $buffer['properties'] = $this->getProperties();
+
+            return V5::pack($buffer);
+        }
+
+        return V3::pack($buffer);
+    }
+}
