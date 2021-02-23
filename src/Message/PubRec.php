224570076397d335b22b8cdd4f57ffa5102b6689
sy-records <52o@qq52o.cn>
2021-02-23 16:12:11 +0800

    Add DISCONNECT、PUBACK、PUBREC、PUBREL、UNSUBACK

---
+++ src/Message/PubRec.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * This file is part of Simps
+ *
+ * @link     https://github.com/simps/mqtt
+ * @contact  Lu Fei <lufei@simps.io>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code
+ */
+
+declare(strict_types=1);
+
+namespace Simps\MQTT\Message;
+
+use Simps\MQTT\Hex\ReasonCode;
+use Simps\MQTT\Protocol\Types;
+use Simps\MQTT\Protocol\V3;
+use Simps\MQTT\Protocol\V5;
+
+class PubRec extends AbstractMessage
+{
+    protected $messageId = 0;
+
+    protected $code = ReasonCode::SUCCESS;
+
+    public function getMessageId(): int
+    {
+        return $this->messageId;
+    }
+
+    public function setMessageId(int $messageId): self
+    {
+        $this->messageId = $messageId;
+
+        return $this;
+    }
+
+    public function getCode(): int
+    {
+        return $this->code;
+    }
+
+    public function setCode(int $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        $buffer = [
+            'type' => Types::PUBREC,
+            'message_id' => $this->getMessageId(),
+        ];
+
+        if ($this->isMQTT5()) {
+            $buffer['code'] = $this->getCode();
+            $buffer['properties'] = $this->getProperties();
+
+            return V5::pack($buffer);
+        }
+
+        return V3::pack($buffer);
+    }
+}
