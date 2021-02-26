e92b11148f55124f47d9f926d2dedc3dc4cf3d67
sy-records <52o@qq52o.cn>
2021-02-26 16:54:57 +0800

    Add visibility to const constants

--- src/Hex/Property.php
+++ src/Hex/Property.php
@@ -18,57 +18,57 @@ namespace Simps\MQTT\Hex;
  */
 class Property
 {
-    const PAYLOAD_FORMAT_INDICATOR = 0x01;
+    public const PAYLOAD_FORMAT_INDICATOR = 0x01;
 
-    const MESSAGE_EXPIRY_INTERVAL = 0x02;
+    public const MESSAGE_EXPIRY_INTERVAL = 0x02;
 
-    const CONTENT_TYPE = 0x03;
+    public const CONTENT_TYPE = 0x03;
 
-    const RESPONSE_TOPIC = 0x08;
+    public const RESPONSE_TOPIC = 0x08;
 
-    const CORRELATION_DATA = 0x09;
+    public const CORRELATION_DATA = 0x09;
 
-    const SUBSCRIPTION_IDENTIFIER = 0x0B;
+    public const SUBSCRIPTION_IDENTIFIER = 0x0B;
 
-    const SESSION_EXPIRY_INTERVAL = 0x11;
+    public const SESSION_EXPIRY_INTERVAL = 0x11;
 
-    const ASSIGNED_CLIENT_IDENTIFIER = 0x12;
+    public const ASSIGNED_CLIENT_IDENTIFIER = 0x12;
 
-    const SERVER_KEEP_ALIVE = 0x13;
+    public const SERVER_KEEP_ALIVE = 0x13;
 
-    const AUTHENTICATION_METHOD = 0x15;
+    public const AUTHENTICATION_METHOD = 0x15;
 
-    const AUTHENTICATION_DATA = 0x16;
+    public const AUTHENTICATION_DATA = 0x16;
 
-    const REQUEST_PROBLEM_INFORMATION = 0x17;
+    public const REQUEST_PROBLEM_INFORMATION = 0x17;
 
-    const WILL_DELAY_INTERVAL = 0x18;
+    public const WILL_DELAY_INTERVAL = 0x18;
 
-    const REQUEST_RESPONSE_INFORMATION = 0x19;
+    public const REQUEST_RESPONSE_INFORMATION = 0x19;
 
-    const RESPONSE_INFORMATION = 0x1A;
+    public const RESPONSE_INFORMATION = 0x1A;
 
-    const SERVER_REFERENCE = 0x1C;
+    public const SERVER_REFERENCE = 0x1C;
 
-    const REASON_STRING = 0x1F;
+    public const REASON_STRING = 0x1F;
 
-    const RECEIVE_MAXIMUM = 0x21;
+    public const RECEIVE_MAXIMUM = 0x21;
 
-    const TOPIC_ALIAS_MAXIMUM = 0x22;
+    public const TOPIC_ALIAS_MAXIMUM = 0x22;
 
-    const TOPIC_ALIAS = 0x23;
+    public const TOPIC_ALIAS = 0x23;
 
-    const MAXIMUM_QOS = 0x24;
+    public const MAXIMUM_QOS = 0x24;
 
-    const RETAIN_AVAILABLE = 0x25;
+    public const RETAIN_AVAILABLE = 0x25;
 
-    const USER_PROPERTY = 0x26;
+    public const USER_PROPERTY = 0x26;
 
-    const MAXIMUM_PACKET_SIZE = 0x27;
+    public const MAXIMUM_PACKET_SIZE = 0x27;
 
-    const WILDCARD_SUBSCRIPTION_AVAILABLE = 0x28;
+    public const WILDCARD_SUBSCRIPTION_AVAILABLE = 0x28;
 
-    const SUBSCRIPTION_IDENTIFIER_AVAILABLE = 0x29;
+    public const SUBSCRIPTION_IDENTIFIER_AVAILABLE = 0x29;
 
-    const SHARED_SUBSCRIPTION_AVAILABLE = 0x2A;
+    public const SHARED_SUBSCRIPTION_AVAILABLE = 0x2A;
 }
--- src/Hex/ReasonCode.php
+++ src/Hex/ReasonCode.php
@@ -18,95 +18,95 @@ namespace Simps\MQTT\Hex;
  */
 abstract class ReasonCode
 {
-    const SUCCESS = 0x00;
+    public const SUCCESS = 0x00;
 
-    const NORMAL_DISCONNECTION = 0x00;
+    public const NORMAL_DISCONNECTION = 0x00;
 
-    const GRANTED_QOS_0 = 0x00;
+    public const GRANTED_QOS_0 = 0x00;
 
-    const GRANTED_QOS_1 = 0x01;
+    public const GRANTED_QOS_1 = 0x01;
 
-    const GRANTED_QOS_2 = 0x02;
+    public const GRANTED_QOS_2 = 0x02;
 
-    const DISCONNECT_WITH_WILL_MESSAGE = 0x04;
+    public const DISCONNECT_WITH_WILL_MESSAGE = 0x04;
 
-    const NO_MATCHING_SUBSCRIBERS = 0x10;
+    public const NO_MATCHING_SUBSCRIBERS = 0x10;
 
-    const NO_SUBSCRIPTION_EXISTED = 0x11;
+    public const NO_SUBSCRIPTION_EXISTED = 0x11;
 
-    const CONTINUE_AUTHENTICATION = 0x18;
+    public const CONTINUE_AUTHENTICATION = 0x18;
 
-    const RE_AUTHENTICATE = 0x19;
+    public const RE_AUTHENTICATE = 0x19;
 
-    const UNSPECIFIED_ERROR = 0x80;
+    public const UNSPECIFIED_ERROR = 0x80;
 
-    const MALFORMED_PACKET = 0x81;
+    public const MALFORMED_PACKET = 0x81;
 
-    const PROTOCOL_ERROR = 0x82;
+    public const PROTOCOL_ERROR = 0x82;
 
-    const IMPLEMENTATION_SPECIFIC_ERROR = 0x83;
+    public const IMPLEMENTATION_SPECIFIC_ERROR = 0x83;
 
-    const UNSUPPORTED_PROTOCOL_VERSION = 0x84;
+    public const UNSUPPORTED_PROTOCOL_VERSION = 0x84;
 
-    const CLIENT_IDENTIFIER_NOT_VALID = 0x85;
+    public const CLIENT_IDENTIFIER_NOT_VALID = 0x85;
 
-    const BAD_USER_NAME_OR_PASSWORD = 0x86;
+    public const BAD_USER_NAME_OR_PASSWORD = 0x86;
 
-    const NOT_AUTHORIZED = 0x87;
+    public const NOT_AUTHORIZED = 0x87;
 
-    const SERVER_UNAVAILABLE = 0x88;
+    public const SERVER_UNAVAILABLE = 0x88;
 
-    const SERVER_BUSY = 0x89;
+    public const SERVER_BUSY = 0x89;
 
-    const BANNED = 0x8A;
+    public const BANNED = 0x8A;
 
-    const SERVER_SHUTTING_DOWN = 0x8B;
+    public const SERVER_SHUTTING_DOWN = 0x8B;
 
-    const BAD_AUTHENTICATION_METHOD = 0x8C;
+    public const BAD_AUTHENTICATION_METHOD = 0x8C;
 
-    const KEEP_ALIVE_TIMEOUT = 0x8D;
+    public const KEEP_ALIVE_TIMEOUT = 0x8D;
 
-    const SESSION_TAKEN_OVER = 0x8E;
+    public const SESSION_TAKEN_OVER = 0x8E;
 
-    const TOPIC_FILTER_INVALID = 0x8F;
+    public const TOPIC_FILTER_INVALID = 0x8F;
 
-    const TOPIC_NAME_INVALID = 0x90;
+    public const TOPIC_NAME_INVALID = 0x90;
 
-    const PACKET_IDENTIFIER_IN_USE = 0x91;
+    public const PACKET_IDENTIFIER_IN_USE = 0x91;
 
-    const PACKET_IDENTIFIER_NOT_FOUND = 0x92;
+    public const PACKET_IDENTIFIER_NOT_FOUND = 0x92;
 
-    const RECEIVE_MAXIMUM_EXCEEDED = 0x93;
+    public const RECEIVE_MAXIMUM_EXCEEDED = 0x93;
 
-    const TOPIC_ALIAS_INVALID = 0x94;
+    public const TOPIC_ALIAS_INVALID = 0x94;
 
-    const PACKET_TOO_LARGE = 0x95;
+    public const PACKET_TOO_LARGE = 0x95;
 
-    const MESSAGE_RATE_TOO_HIGH = 0x96;
+    public const MESSAGE_RATE_TOO_HIGH = 0x96;
 
-    const QUOTA_EXCEEDED = 0x97;
+    public const QUOTA_EXCEEDED = 0x97;
 
-    const ADMINISTRATIVE_ACTION = 0x98;
+    public const ADMINISTRATIVE_ACTION = 0x98;
 
-    const PAYLOAD_FORMAT_INVALID = 0x99;
+    public const PAYLOAD_FORMAT_INVALID = 0x99;
 
-    const RETAIN_NOT_SUPPORTED = 0x9A;
+    public const RETAIN_NOT_SUPPORTED = 0x9A;
 
-    const QOS_NOT_SUPPORTED = 0x9B;
+    public const QOS_NOT_SUPPORTED = 0x9B;
 
-    const USE_ANOTHER_SERVER = 0x9C;
+    public const USE_ANOTHER_SERVER = 0x9C;
 
-    const SERVER_MOVED = 0x9D;
+    public const SERVER_MOVED = 0x9D;
 
-    const SHARED_SUBSCRIPTIONS_NOT_SUPPORTED = 0x9E;
+    public const SHARED_SUBSCRIPTIONS_NOT_SUPPORTED = 0x9E;
 
-    const CONNECTION_RATE_EXCEEDED = 0x9F;
+    public const CONNECTION_RATE_EXCEEDED = 0x9F;
 
-    const MAXIMUM_CONNECT_TIME = 0xA0;
+    public const MAXIMUM_CONNECT_TIME = 0xA0;
 
-    const SUBSCRIPTION_IDENTIFIERS_NOT_SUPPORTED = 0xA1;
+    public const SUBSCRIPTION_IDENTIFIERS_NOT_SUPPORTED = 0xA1;
 
-    const WILDCARD_SUBSCRIPTIONS_NOT_SUPPORTED = 0xA2;
+    public const WILDCARD_SUBSCRIPTIONS_NOT_SUPPORTED = 0xA2;
 
     /**
      * @see https://docs.oasis-open.org/mqtt/mqtt/v5.0/os/mqtt-v5.0-os.html#_Toc3901079
