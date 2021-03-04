56fc2df4212284cf64b4a0f9d8a5d79a306a5cd1
沈唁 <52o@qq52o.cn>
2021-03-04 10:56:22 +0800

    Add WillMessage (#45)

---
+++ src/Message/Will.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * This file is part of Simps
+ *
+ * @link     https://github.com/simps/mqtt
+ * @contact  Lu Fei <lufei@simps.io>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code
+ */
+
+declare(strict_types=1);
+
+namespace Simps\MQTT\Message;
+
+use Simps\MQTT\Protocol\ProtocolInterface;
+
+class Will extends AbstractMessage
+{
+    protected $topic = '';
+
+    protected $qos = ProtocolInterface::MQTT_QOS_0;
+
+    protected $retain = 0;
+
+    protected $message = '';
+
+    public function getTopic(): string
+    {
+        return $this->topic;
+    }
+
+    public function setTopic(string $topic): self
+    {
+        $this->topic = $topic;
+
+        return $this;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getQos(): int
+    {
+        return $this->qos;
+    }
+
+    public function setQos(int $qos): self
+    {
+        $this->qos = $qos;
+
+        return $this;
+    }
+
+    public function getRetain(): int
+    {
+        return $this->retain;
+    }
+
+    public function setRetain(int $retain): self
+    {
+        $this->retain = $retain;
+
+        return $this;
+    }
+
+    public function getContents(bool $getArray = false)
+    {
+        $buffer = [
+            'topic' => $this->getTopic(),
+            'qos' => $this->getQos(),
+            'retain' => $this->getRetain(),
+            'message' => $this->getMessage(),
+        ];
+
+        if ($this->isMQTT5()) {
+            $buffer['properties'] = $this->getProperties();
+        }
+
+        if ($getArray) {
+            return $buffer;
+        }
+
+        // The will message can only be an array
+        return '';
+    }
+}
