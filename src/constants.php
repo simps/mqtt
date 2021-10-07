418a2e4a38c869f039b55713bae4edc5ed0dc9c5
sy-records <52o@qq52o.cn>
2021-10-07 08:51:58 +0800

    Add mqtt constants

---
+++ src/constants.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * This file is part of Simps
+ *
+ * @link     https://github.com/simps/mqtt
+ * @contact  Lu Fei <lufei@simps.io>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code
+ */
+
+declare(strict_types=1);
+
+define('MQTT_PROTOCOL_LEVEL_3_1', 3);
+define('MQTT_PROTOCOL_LEVEL_3_1_1', 4);
+define('MQTT_PROTOCOL_LEVEL_5_0', 5);
+define('MQISDP_PROTOCOL_NAME', 'MQIsdp');
+define('MQTT_PROTOCOL_NAME', 'MQTT');
+define('MQTT_QOS_0', 0);
+define('MQTT_QOS_1', 1);
+define('MQTT_QOS_2', 2);
+define('MQTT_RETAIN_0', 0);
+define('MQTT_RETAIN_1', 1);
+define('MQTT_RETAIN_2', 2);
+define('MQTT_DUP_0', 0);
+define('MQTT_DUP_1', 1);
+define('MQTT_SESSION_PRESENT_0', 0);
+define('MQTT_SESSION_PRESENT_1', 1);
+define('MQTT_TYPE_CONNECT', 1);
+define('MQTT_TYPE_CONNACK', 2);
+define('MQTT_TYPE_PUBLISH', 3);
+define('MQTT_TYPE_PUBACK', 4);
+define('MQTT_TYPE_PUBREC', 5);
+define('MQTT_TYPE_PUBREL', 6);
+define('MQTT_TYPE_PUBCOMP', 7);
+define('MQTT_TYPE_SUBSCRIBE', 8);
+define('MQTT_TYPE_SUBACK', 9);
+define('MQTT_TYPE_UNSUBSCRIBE', 10);
+define('MQTT_TYPE_UNSUBACK', 11);
+define('MQTT_TYPE_PINGREQ', 12);
+define('MQTT_TYPE_PINGRESP', 13);
+define('MQTT_TYPE_DISCONNECT', 14);
+define('MQTT_TYPE_AUTH', 15);
+define('MQTT_VERBOSE_NONE', 0);
+define('MQTT_VERBOSE_HEXDUMP', 1);
+define('MQTT_VERBOSE_HEXDUMP_ASCII', 2);
+define('MQTT_VERBOSE_ASCII', 3);
+define('MQTT_VERBOSE_TEXT', 4);
+define('MQTT_VERBOSE_HEX_STREAM', 5);
