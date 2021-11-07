0cdff9cf77fee450062c5d010c4ccd8bc17a292f
sy-records <52o@qq52o.cn>
2021-11-07 09:30:34 +0800

    Update header_comment

--- tests/Unit/ClientTest.php
+++ tests/Unit/ClientTest.php
@@ -1,13 +1,4 @@
 <?php
-/**
- * This file is part of Simps
- *
- * @see     https://github.com/simps/mqtt
- * @contact  Lu Fei <lufei@simps.io>
- *
- * For the full copyright and license information,
- * please view the LICENSE file that was distributed with this source code
- */
 
 declare(strict_types=1);
 /**
