7c4c2484cd59a50a6e4716545a8b6c3b65b30a90
sy-records <52o@qq52o.cn>
2022-08-01 08:52:05 +0800

    Fix error in calculating properties length

--- tests/Unit/ClientTest.php
+++ tests/Unit/ClientTest.php
@@ -40,12 +40,14 @@ class ClientTest extends TestCase
     {
         $topic = 'simps-mqtt/test/base64';
         $base64 = base64_encode(file_get_contents(TESTS_DIR . '/files/wechat.jpg'));
-        $client = new MQTTClient(SIMPS_MQTT_REMOTE_HOST, SIMPS_MQTT_PORT, getTestConnectConfig());
+        $config = getTestConnectConfig();
+        $config->setSwooleConfig(['read_timeout' => 10.0]);
+        $client = new MQTTClient('test.mosquitto.org', SIMPS_MQTT_PORT, $config);
         $client->connect(false);
         $client->subscribe([$topic => 0]);
 
         Coroutine::create(function () use ($topic, $base64) {
-            $client = new MQTTClient(SIMPS_MQTT_REMOTE_HOST, SIMPS_MQTT_PORT, getTestConnectConfig());
+            $client = new MQTTClient('test.mosquitto.org', SIMPS_MQTT_PORT, getTestConnectConfig());
             $client->connect();
             $client->publish($topic, $base64);
         });
