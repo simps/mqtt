4298481f646111af40e442440cb06a29aab8fe8b
Luffy <52o@qq52o.cn>
2026-04-28 14:18:13 +0800

    Increase MQTT client timeout settings and update test configuration to use remote host

--- tests/Unit/ClientTest.php
+++ tests/Unit/ClientTest.php
@@ -40,20 +40,18 @@ class ClientTest extends TestCase
     {
         $topic = 'simps-mqtt/test/base64';
         $base64 = base64_encode(file_get_contents(TESTS_DIR . '/files/wechat.jpg'));
-        $config = getTestConnectConfig();
-        $config->setSwooleConfig(['read_timeout' => 10, 'connect_timeout' => 10, 'write_timeout' => 10]);
-        $client = new MQTTClient(SIMPS_MQTT_MOSQUITTO, SIMPS_MQTT_PORT, $config);
+        $client = new MQTTClient(SIMPS_MQTT_REMOTE_HOST, SIMPS_MQTT_PORT, getTestConnectConfig());
         $client->connect(false);
         $client->subscribe([$topic => 0]);
 
         Coroutine::create(function () use ($topic, $base64) {
-            $client = new MQTTClient(SIMPS_MQTT_MOSQUITTO, SIMPS_MQTT_PORT, getTestConnectConfig());
+            $client = new MQTTClient(SIMPS_MQTT_REMOTE_HOST, SIMPS_MQTT_PORT, getTestConnectConfig());
             $client->connect();
             $client->publish($topic, $base64);
         });
 
         $buffer = $client->recv();
-        if (empty($buffer)){
+        if (empty($buffer)) {
             $buffer = $client->recv();
         }
         $this->assertSame(Types::PUBLISH, $buffer['type']);
